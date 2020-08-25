3bd84b163cfb94c7ee60c014c6b796cd33d35a62
akash-12398 <napinderbajwa@gmail.com>
2020-08-25 14:12:50 -0500

    first commit

---
+++ SwitchStatementsExcercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchStatementsExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
